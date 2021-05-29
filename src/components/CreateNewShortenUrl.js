fb7a4093a53ef93539bc135a45f3e688cd113e0b
AWINAS-KANNAN-2719 <awinas270597@gmail.com>
2021-05-30 00:00:27 +0530

    Css Added

--- src/components/CreateNewShortenUrl.js
+++ src/components/CreateNewShortenUrl.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-
+import '../index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export function CreateNewShortenUrl() {
 
@@ -10,12 +11,12 @@ export function CreateNewShortenUrl() {
         setUrl(e.target.value);
     };
 
-    
+
     function createNewShortUrl() {
         if (url === '') {
             alert('Enter Url Value')
         } else {
-            fetch('http://localhost:8080/urlshortner/shorten?origUrl='+url
+            fetch('http://localhost:8080/urlshortner/shorten?origUrl=' + url
                 , {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' }
@@ -28,38 +29,64 @@ export function CreateNewShortenUrl() {
 
 
     return (
-        <div>
-            <h3>Create New Shorten Url</h3>
-            <p>
-                <label><b>New url :  </b> </label>
-                <input type="text" name="url" value={url} onChange={onUrlTextChange} />
-            </p>
+        <div className='container' style={{ marginTop: '40px' }}>
+            <div className='row'>
+                <div className=' create-container col-lg-9 offset-lg-2'>
+                    <div className='row justify-content-center' style={{ paddingTop: '30px' }}>
+                        <div className='col-lg-8 offset-lg-3'>
+                            <h4>Create New Shorten Url</h4>
+                        </div>
+                    </div>
+                    <div className='row'>
+                        <div className='offset-lg-3 col-lg-2'>
+                            <label style={{ paddingTop: '5px' }}><b>Actual url :  </b> </label>
+                        </div>
+                        <div className='col-lg-4'>
+                            <input className='input-background Input-text-label' type="text" name="url" value={url} onChange={onUrlTextChange} />
+                        </div>
+                    </div>
+                    <div className='row' style={{ paddingTop: '20px' }}>
+                        <div className='offset-lg-4 col-lg-4'>
+                            <button className='create-btn-background' style={{ paddingTop: '0px !important' }} onClick={createNewShortUrl}>
+                                <span className='create-btn-label'>Create</span>
+                            </button>
+                        </div>
+                    </div>
+                    {shortUrlResponse.httpCode === "200" &&
+                        <div>
+                            <div className='row' style={{ paddingTop: '20px' }}>
+                                <div className='offset-lg-3 col-lg-4'>
+                                    {shortUrlResponse.responseDescription}
+                                </div>
+                            </div>
+                            <div className='row' style={{ paddingTop: '10px' }}>
+                                <div className='offset-lg-3 col-lg-2'>
+                                    <label><b>Original Url :  </b> </label>
+                                </div>
+                                {shortUrlResponse.originalUrl}
 
-            <button onClick={createNewShortUrl}>Add New Url</button>
+                            </div>
+                            <div className='row' style={{ paddingTop: '10px' }}>
+                                <div className='offset-lg-3 col-lg-2'>
+                                    <label><b>Shorten url :  </b> </label>
+                                </div>
+                                {shortUrlResponse.shortUrl}
 
-            {shortUrlResponse.httpCode === "200" &&
-                <div>
-                    <p>
-                        {shortUrlResponse.responseDescription}
-                    </p>
-                    <p>
-                        <label><b>Original Url :  </b> </label>
-                        {shortUrlResponse.originalUrl}
-                    </p>
-                    <p>
-                        <label><b>Shorten url :  </b> </label>
-                        {shortUrlResponse.shortUrl}
-                    </p>
+                            </div>
+                        </div>
+
+                    }
+                    {shortUrlResponse.httpCode === "400" &&
+
+                        <div className='row' style={{ paddingTop: '20px' }}>
+                            <div className='offset-lg-3 col-lg-2'>
+                                <label><b>Error :  </b> </label>
+                            </div>
+                            {shortUrlResponse.responseDescription}
+                        </div>
+                    }
                 </div>
-
-            }
-            {shortUrlResponse.httpCode === "400" &&
-
-                <p>
-                    <label><b>Error detail :  </b> </label>
-                    {shortUrlResponse.responseDescription}
-                </p>
-            }
+            </div>
         </div>
     )
 
--- src/components/UrlShortner.js
+++ src/components/UrlShortner.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { CreateNewShortenUrl } from "./CreateNewShortenUrl";
 import { ShowAllUrl } from "./ShowAllUrl";
-
+import '../index.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export function UrlShortner() {
     var [urlComponents, setUrlComponentValues] = useState({ showCreateComp: false, showAllUrlComp: false });
@@ -12,7 +13,7 @@ export function UrlShortner() {
                 showCreateComp: true,
                 showAllUrlComp: false
             });
-        } else  if (e.target.name === "showAllUrlComponent") {
+        } else if (e.target.name === "showAllUrlComponent") {
             setUrlComponentValues({
                 showCreateComp: false,
                 showAllUrlComp: true
@@ -21,15 +22,23 @@ export function UrlShortner() {
 
     }
     return (
-        <div>
-            <h2>Welcome to Url Shortner Application...</h2>
-            <p>
-            <button name='createcomponent' onClick={setShowOrHideComponent}>Create New shorten Url</button>
-             
-            <button name='showAllUrlComponent' onClick={setShowOrHideComponent}>Show All Urls</button>
-            </p>
-            { urlComponents.showCreateComp ? <CreateNewShortenUrl/>: null}
-            { urlComponents.showAllUrlComp ? <ShowAllUrl data={urlComponents}/> : null}
+        <div className='container-fluid'>
+            <div className='row justify-content-center'>
+            <div className='col-lg-8 offset-lg-4'> 
+                <h2>Url Shortner Application</h2>
+            </div>   
+            </div>
+
+            <div className='row ' style={{paddingTop:"20px"}}>
+                <div className='offset-lg-2 col-lg-4'>
+                    <button className='button' name='createcomponent' onClick={setShowOrHideComponent}>Create New shorten Url</button>
+                </div>
+                <div className='offset-lg-1 col-lg-4'>
+                    <button className='button' name='showAllUrlComponent' onClick={setShowOrHideComponent}>Show All Urls</button>
+                </div>
+            </div>
+            { urlComponents.showCreateComp ? <CreateNewShortenUrl /> : null}
+            { urlComponents.showAllUrlComp ? <ShowAllUrl data={urlComponents} /> : null}
         </div>
     )
 }
